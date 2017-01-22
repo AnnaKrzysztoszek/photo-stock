51d83e903294079bbd79381e5b0033bec182bf7b
anna <krzysztoszek.a@gmail.com>
2017-01-22 14:32:53 +0100

    JDBC client repo

--- src/pl/com/bottega/photostock/sales/model/money/IntegerMoney.java
+++ src/pl/com/bottega/photostock/sales/model/money/IntegerMoney.java
@@ -3,12 +3,12 @@ package pl.com.bottega.photostock.sales.model.money;
 /**
  * Created by anna on 18.12.2016.
  */
-class IntegerMoney implements Money {
+public class IntegerMoney implements Money {
 
     private long cents;
     private Currency currency;
 
-    IntegerMoney(long cents, Currency currency) {
+    public IntegerMoney(long cents, Currency currency) {
         this.cents = cents;
         this.currency = currency;
     }
